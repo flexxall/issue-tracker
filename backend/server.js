54e3d3a8400b45495db06b666a559cb73fc27f38
flexxall <flexxall@live.com>
2021-11-03 18:20:31 -0500

    Preparing for deplayement

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/connectDB.js";
 import userRouter from "./routes/userRouter.js";
 import issueRouter from "./routes/issueRouter.js";
 import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
+import path from "path";
 
 const app = express();
 
@@ -17,6 +18,24 @@ connectDB();
 app.use("/users", userRouter);
 app.use("/issues", issueRouter);
 app.use("/myIssues", issueRouter);
+
+// -------------------- Deployement --------------------
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// -------------------- Deployement --------------------
 
 app.use(notFound);
 app.use(errorHandler);
